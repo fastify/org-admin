c1124f485094493e8910d0dd317c75ef06793892
Manuel Spigolon <manuel.spigolon@nearform.com>
2025-10-05 16:11:08 +0200

    chore: npm logs

--- commands/onboard.js
+++ commands/onboard.js
@@ -32,6 +32,20 @@ export default async function onboard ({ client, logger }, { org, username, join
       logger.info('Added %s to team %s', joiningUser.login, targetTeam.slug)
     }
   }
+
+  logger.info('GitHub onboarding completed for user %s ✅ ', joiningUser.login)
+
+  logger.warn('To complete the NPM onboarding, please following these steps:')
+  // This step cannot be automated, there are no API to add members to an org on NPM
+  logger.info('1. Invite the user to the organization on NPM: https://www.npmjs.com/org/%s/invite?track=existingOrgAddMembers', org)
+  logger.info('2. Add the user to the relevant teams by using the commands:');
+  [
+    { slug: 'developers' }, // NPM has a default team for every org
+    ...destinationTeams
+  ].forEach(team => {
+    logger.info('npm team add @%s:%s %s', org, team.slug, joiningUser.login)
+  })
+  logger.info('When it will be done, the NPM onboarding will be completed for user %s ✅ ', joiningUser.login)
 }
 
 async function confirm (q) {
